8176d0c5c9841fd9aea1e3b9edf2e274693fa4c1
Aaditya Paudel <paudelaaditya966@gmail.com>
2025-06-05 00:09:22 +0545

    Initial commit

--- TheLastTree/code/Spritesheet.py
+++ TheLastTree/code/Spritesheet.py
@@ -2,8 +2,6 @@ import pygame
 class SpriteSheet():
     def __init__(self,image):
         self.sheet= image
-        
-#Segment sprite sheet
 #Segment sprite sheet
 def get_image(self,frame, width, height, scale, colour):
     image = pygame.Surface((width, height), pygame.SRCALPHA).convert_alpha()
